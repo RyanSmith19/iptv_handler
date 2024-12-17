5af5e556a0a212846a2473d82fe8baf9a2a3018b
RyanSmith19 <lifewithryans@gmail.com>
2024-12-16 23:01:00 -0800

    Initial Commit

---
+++ iptv_handler/iptv_handlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iptv_handlerApp.swift
+//  iptv_handler
+//
+//  Created by Ryan Smith on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct iptv_handlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iptv_handlerTests/iptv_handlerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iptv_handlerTests.swift
+//  iptv_handlerTests
+//
+//  Created by Ryan Smith on 12/16/24.
+//
+
+import Testing
+@testable import iptv_handler
+
+struct iptv_handlerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
